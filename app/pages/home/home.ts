913a96ee3d7d77207789d0160980823f4d3c06e4
Ivan Larionov <ivanthecrazy@MacBook-Ivan.local>
2016-08-01 23:02:57 +0400

    almost done with example

--- app/pages/home/home.ts
+++ app/pages/home/home.ts
@@ -1,11 +1,29 @@
 import {Component} from '@angular/core';
+import {Camera} from 'ionic-native';
 import {NavController} from 'ionic-angular';
 
 @Component({
   templateUrl: 'build/pages/home/home.html'
 })
 export class HomePage {
+  cameraData: string;
+  photoTaken: boolean;
+
   constructor(private navCtrl: NavController) {
-  
+    this.photoTaken = false;
+  }
+
+  openCamera() {
+    var options = {
+      sourceType: Camera.PictureSourceType.CAMERA
+    };
+    Camera.getPicture(options).then((imageData) => {
+      // imageData is either a base64 encoded string or a file URI
+      // If it's base64:
+      this.cameraData = 'data:image/jpeg;base64,' + imageData;
+      this.photoTaken = true;
+    }, (err) => {
+      // Handle error
+    });
   }
 }
